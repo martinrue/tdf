f03ce19bd141a84796bc9b387eacd22286e02966
Elizabeth Kershaw <elizabethkershaw7@gmail.com>
2024-03-07 00:59:39 +0700

    Update fund to 6th March

--- js/entries.js
+++ js/entries.js
@@ -75,10 +75,89 @@ const entries = [
       {
         type: "gallery",
         cost: 492,
-        title: "📸 &nbsp;We're loving Dubai so far",
+        title: "📸 &nbsp;We're loving it here so far.",
         data: {
           id: "9e6340669a5d47a1af3ec80c7d868191",
           images: [{ url: "/entries/24-02-27/1.jpeg" }, { url: "/entries/24-02-27/2.jpeg" }, { url: "/entries/24-02-27/3.jpeg" }, { url: "/entries/24-02-27/4.jpeg" }, { url: "/entries/24-02-27/5.jpeg" }, { url: "/entries/24-02-27/6.jpeg" }],
+        },
+      },
+    ],
+  },
+  {
+    date: "2024-02-29",
+    authors: ["martin", "elizabeth"],
+    items: [
+      {
+        type: "youtube",
+        cost: 0,
+        title: "🎥 &nbsp;First time in Dubai",
+        data: {
+          body: "This city surprised us and it wasn't what we expected. Check out what we got up to.",
+          thumbnail: "/entries/24-02-29/vlog.jpeg",
+          url: "https://www.youtube.com/watch?v=WdNrDEDMvhM",
+        },
+      },
+      {
+        type: "gallery",
+        cost: 0,
+        title: "📸 &nbsp;More city highlights!",
+        data: {
+          id: "3da457dcace74cebac203add5bfc8aa9",
+          images: [{ url: "/entries/24-02-29/1.jpeg" }, { url: "/entries/24-02-29/2.jpeg" }, { url: "/entries/24-02-29/3.jpeg" }, { url: "/entries/24-02-29/4.jpeg" }, { url: "/entries/24-02-29/6.jpeg" }, { url: "/entries/24-02-29/5.jpeg" }],
+        },
+      },
+      {
+        type: "text",
+        cost: 0,
+        title: "⚡️ Our thoughts on Dubai",
+        data: {
+          body: "We can't lie, Dubai impressed us more than we expected. From its super clean streets and architectural wonder, to the great food and relaxed vibes, this city has a lot going for it. In the end we feel like we could have spent a bit more time here – but who knows, maybe we'll be back.",
+        },
+      },
+    ],
+  },
+  {
+    date: "2024-03-01",
+    authors: ["martin", "elizabeth"],
+    items: [
+      {
+        type: "tickets",
+        cost: 791,
+        title: "✈️ &nbsp;Next stop: Hanoi",
+        data: {
+          url: "/entries/24-03-01/ticket.png",
+        },
+      },
+      {
+        type: "image",
+        cost: 0,
+        title: "📍 Good morning Vietnam!",
+        data: {
+          url: "/entries/24-03-01/dxb-han.jpg",
+        },
+      },
+      {
+        type: "gallery",
+        cost: 539,
+        title: "📸 &nbsp;Our home in Hanoi",
+        data: {
+          id: "11adcc070c5f4efb8eebbed2dc41b034",
+          images: [{ url: "/entries/24-03-01/1.jpeg" }, { url: "/entries/24-03-01/2.jpeg" }, { url: "/entries/24-03-01/3.jpeg" }],
+        },
+      },
+    ],
+  },
+  {
+    date: "2024-03-06",
+    authors: ["martin", "elizabeth"],
+    items: [
+      {
+        type: "gallery",
+        cost: 400,
+        title: "📸 &nbsp;The food in Hanoi is awesome",
+        data: {
+          id: "53fbb6e02cf4464a8cbd96973b3001c2",
+          images: [{ url: "/entries/24-03-06/1.jpeg" }, { url: "/entries/24-03-06/2.jpeg" }, { url: "/entries/24-03-06/3.jpeg" }, { url: "/entries/24-03-06/4.jpeg" }],
         },
       },
     ],
